1706cbc9efc5b1777b138978588ce623732c5c5a
janitochvvll2311 <janitochvvll2311@gmail.com>
2024-03-11 18:27:32 -0600

    Html templates

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-import { WebUrl } from "./lib/web-url.js";
-import {fetchCss, fetchHtml} from "./lib/import.js";
+import { fetchHtml } from "./lib/import.js";
 
-console.log(await fetchHtml("index.html"));
-console.log(await fetchCss("main.css"));
+document.getElementsByTagName("header")[0]
+    .replaceWith(await fetchHtml("/html/header.html"));
+
+document.getElementsByTagName("nav")[0]
+    .replaceWith(await fetchHtml("/html/navigation.html"));
