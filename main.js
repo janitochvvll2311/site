45650be4b45c94c8dbdaa0d0936aad96e01e6d17
janitochvvll2311 <janitochvvll2311@gmail.com>
2024-03-11 18:40:56 -0600

    Example of entries

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
-import { fetchHtml } from "./lib/import.js";
+import { fetchHtml, fetchJson } from "./lib/import.js";
 
 document.getElementsByTagName("header")[0]
     .replaceWith(await fetchHtml("/html/header.html"));
 
 document.getElementsByTagName("nav")[0]
     .replaceWith(await fetchHtml("/html/navigation.html"));
+
+let index = await fetchJson("/data/posts/index.json");
+let html = await fetchHtml(`/data/posts/${index[0].file}`);
+document.body.appendChild(html);
